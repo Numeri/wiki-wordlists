3c5a6bed104e25ec24c257fe8c524eeb8191682d
Numeri <Numeri@users.noreply.github.com>
2019-06-30 17:43:12 -0400

    Initial working program
    A python program to construct word frequency lists from Wikipedia
    pages. Input is a .xml file as downloaded from
    https://en.wikipedia.org/wiki/Special:Export, the equivalent for
    a different language or the complete xml structure for an entire
    wiki, as downloaded from
    https://dumps.wikimedia.org/backup-index.html
    
    An additional optional parameter is the number of words in the
    frequency list, with a default of 1000 words. If you just want the
    top 10 words, for instance, it would look like this:
    	python extract_wordlist.py filename.xml 10
    
    Current issues are that it doesn't process serially, so it chokes
    on large files. In addition, a couple markup/html keywords seem to
    sneak through the filters.

---
+++ extract_wordlist.py
@@ -0,0 +1,64 @@
+#!/bin/python
+
+from bs4 import BeautifulSoup, Tag
+from collections import Counter
+import regex
+import string
+from sys import argv, maxunicode
+import unicodedata
+
+#Check that arguments were provided
+words_to_output = 1000
+if len(argv) < 2:
+	print('Error: Provide a filename to read and optionally the number of words to output')
+	exit()
+if len(argv) == 3:
+	words_to_output = int(argv[2])
+
+punc = '=|'
+
+for i in range(maxunicode):
+	char = chr(i)
+	if unicodedata.category(char).startswith('P'):
+		punc += char
+punctuation = set(punc)
+punctuation.remove("'")
+
+with open(argv[1]) as xmlfile:
+	xml = xmlfile.read()
+
+soup = BeautifulSoup(xml, 'lxml')
+text_tags = soup.find_all('text')
+
+text = ''
+
+for tag in text_tags:
+	text += tag.get_text()
+
+whitespace_regex = regex.compile('\s+')
+triplequotes_regex = regex.compile(r'\'\'\'')
+cleanup_regex = regex.compile(r"""\[\[.+\]\]|\{\{.+\}\}		# markup tags like [[]] or {{}}
+                                  | (\n|^)[ |*{!=}].*		# tables, links and headers
+                                  | <.*>.*</.*>			# reference tags
+                                  | <.*>.*\n			# reference tags spanning a line
+                                  | .*</.*>			# reference tags spanning a line
+                                  | [0-9]			# numbers
+                               """, regex.VERBOSE)
+
+text = cleanup_regex.sub('', text)
+text = triplequotes_regex.sub(' ', text)
+text = ''.join([ch for ch in text if ch not in punctuation])
+text = whitespace_regex.sub('\n', text)
+
+words = regex.split('\n+', text)
+
+counter = Counter()
+for word in words:
+	counter[word] += 1
+
+counter[''] = 0
+counter["''"] = 0
+counter["'"] = 0
+
+for keypair in counter.most_common(words_to_output):
+	print(keypair[0])
