27760fcd8f10c454761b0910e6c77ae7c4ecfd79
Numeri <Numeri@users.noreply.github.com>
2019-07-27 21:16:20 -0400

    Switched to serial processing of xml
    Now able to process very large files

--- wiki-wordlist.py
+++ wiki-wordlist.py
@@ -1,24 +1,28 @@
 #!/bin/python
 
-from bs4 import BeautifulSoup, Tag
+import xml.etree.ElementTree as etree
 from collections import Counter
 import regex
 import string
 from sys import argv, maxunicode
+from os import fstat
 import unicodedata
+import logging
+
+logging.basicConfig(format='%(message)s')
 
 #Check that arguments were provided
 words_to_output = 1000
 if len(argv) < 2:
-	print('Error: Provide a filename to read and optionally the number of words to output')
+	logging.warning('Error: Provide a filename to read and optionally the number of words to output')
 	exit()
 if len(argv) == 3:
 	words_to_output = int(argv[2])
 	if words_to_output < 1:
-		print('Error: The number of words to output must be more than 0')
+		logging.warning('Error: The number of words to output must be more than 0')
 		exit()
 
-punc = '=|+<>'
+punc = '=|+<>^'
 
 for i in range(maxunicode):
 	char = chr(i)
@@ -28,19 +32,11 @@ punctuation = set(punc)
 punctuation.remove("'")
 
 try:
-	with open(argv[1]) as xmlfile:
-		xml = xmlfile.read()
+	xmlfile = open(argv[1])
+	context = etree.iterparse(xmlfile, events=('start', 'end'))
 except IOError:
-	print('Error: {:s} could not be read'.format(argv[1]))
+	logging.warning('Error: {:s} could not be read'.format(argv[1]))
 	exit()
-
-soup = BeautifulSoup(xml, 'lxml')
-text_tags = soup.find_all('text')
-
-text = ''
-
-for tag in text_tags:
-	text += tag.get_text()
 
 whitespace_regex = regex.compile('\s+')
 quotes_regex = regex.compile(r'\'\'\'?')
@@ -49,19 +45,37 @@ cleanup_regex = regex.compile(r"""\[\[.+\]\]|\{\{.+\}\}		# markup tags like [[]]
                                   | <.*>.*</.*>			# reference tags
                                   | <.*>.*\n			# reference tags spanning a line
                                   | .*</.*>			# reference tags spanning a line
-                                  | [0-9]			# numbers
+                                  | [0-9۰-۹]			# numbers
                                """, regex.VERBOSE)
 
-text = cleanup_regex.sub('', text)
-text = quotes_regex.sub(' ', text)
-text = ''.join([ch for ch in text if ch not in punctuation])
-text = whitespace_regex.sub('\n', text)
-
-words = regex.split('\n+', text)
-
 counter = Counter()
-for word in words:
-	counter[word] += 1
+
+event, root = next(context)
+
+url_length = root.tag.find('}') + 1
+tags_found = 0
+xmlfilesize = float(fstat(xmlfile.fileno()).st_size)
+update_interval = 250 if xmlfilesize > 1e7 else 100
+
+for event, elem in context:
+	if event == 'end' and elem.tag[url_length:] == 'text' and elem.text != None:
+		text = elem.text
+		text = cleanup_regex.sub('', text)
+		text = quotes_regex.sub(' ', text)
+		text = ''.join([ch for ch in text if ch not in punctuation])
+		text = whitespace_regex.sub('\n', text)
+
+		words = regex.split('\n+', text)
+
+		for word in words:
+			counter[word] += 1
+
+		tags_found += 1
+
+		if tags_found % update_interval == 0:
+			logging.warning("Tags found: {0:8n}    Progress: {1:6.4f}".format(tags_found, float(xmlfile.tell())/xmlfilesize))
+
+		root.clear()
 
 counter[''] = 0
 counter["''"] = 0
