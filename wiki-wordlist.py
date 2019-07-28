4586aa5059443ddce6673722170b6800457ee3f2
Numeri <Numeri@users.noreply.github.com>
2019-07-28 19:34:05 -0400

    Added docstring, flags, and config file support

--- wiki-wordlist.py
+++ wiki-wordlist.py
@@ -1,4 +1,16 @@
 #!/bin/python
+"""Wiki Wordlists
+
+Usage:
+  wiki-wordlists.py <file> [options]
+  wiki-wordlists.py -h
+
+Options:
+  -h --help                       Show this screen.
+  -n <number>, --number <number>  Specify number of words [default: 1000]
+  -c <config>, --config <config>  Use config file
+
+"""
 
 import xml.etree.ElementTree as etree
 from collections import Counter
@@ -8,45 +20,58 @@ from sys import argv, maxunicode
 from os import fstat
 import unicodedata
 import logging
+import json
+from docopt import docopt
 
 logging.basicConfig(format='%(message)s')
 
-#Check that arguments were provided
-words_to_output = 1000
-if len(argv) < 2:
-	logging.warning('Error: Provide a filename to read and optionally the number of words to output')
+arguments = docopt(__doc__, version='Wiki Wordlists 0.5')
+
+words_to_output = int(arguments['--number'])
+if words_to_output < 1:
+	logging.warning('Error: The number of words to output must be more than 0')
 	exit()
-if len(argv) == 3:
-	words_to_output = int(argv[2])
-	if words_to_output < 1:
-		logging.warning('Error: The number of words to output must be more than 0')
+
+config_filename = arguments['--config']
+if config_filename != None:
+	try:
+		config_file = open(config_filename, 'r')
+		config = json.load(config_file)
+	except:
+		logging.warning("Error: {0} could not be opened as a config file".format(config_filename))
 		exit()
+else:
+	config = { 'excluded': ['Wikipedia'] }
 
-punc = '=|+<>^'
-
+punc = ''
 for i in range(maxunicode):
 	char = chr(i)
-	if unicodedata.category(char).startswith('P'):
+	if unicodedata.category(char)[0] in 'NPS': # All numbers, punctuation or symbols
 		punc += char
+
 punctuation = set(punc)
-punctuation.remove("'")
+punctuation.remove("'") # Dumb apostrophe
+punctuation.remove("’") # Smart apostrophe
 
 try:
-	xmlfile = open(argv[1])
+	xmlfile = open(arguments['<file>'])
 	context = etree.iterparse(xmlfile, events=('start', 'end'))
 except IOError:
 	logging.warning('Error: {:s} could not be read'.format(argv[1]))
 	exit()
 
 whitespace_regex = regex.compile('\s+')
-quotes_regex = regex.compile(r'\'\'\'?')
-cleanup_regex = regex.compile(r"""\[\[.+\]\]|\{\{.+\}\}		# markup tags like [[]] or {{}}
-                                  | (\n|^)[ |*{!=}].*		# tables, links and headers
-                                  | <.*>.*</.*>			# reference tags
-                                  | <.*>.*\n			# reference tags spanning a line
-                                  | .*</.*>			# reference tags spanning a line
-                                  | [0-9۰-۹]			# numbers
-                               """, regex.VERBOSE)
+quotes_regex = regex.compile(r'\'\'\'?|\n+')
+cleanup_regex = regex.compile(r"""\[\[.+?\]\]|\{\{.+?\}\}	# markup tags like [[]] or {{}}
+                                  | ^[ |*{!=}].*?$		# tables, links and headers
+				  | <!--.*?-->			# comment tags
+                                  | <.*?>.*?</.*?>		# reference tags
+                                  | <.*?>.*?$			# reference tags spanning a line
+                                  | .*?</.*?>			# reference tags spanning a line
+                                  | &lt;.*?&gt;.*?&lt;/.*?&gt;	# reference tags
+                                  | &lt;.*?&gt;.*?\n		# reference tags spanning a line
+                                  | .*?&lt;/.*?&gt;		# reference tags spanning a line
+                               """, regex.VERBOSE | regex.M)
 
 counter = Counter()
 
@@ -55,7 +80,7 @@ event, root = next(context)
 url_length = root.tag.find('}') + 1
 tags_found = 0
 xmlfilesize = float(fstat(xmlfile.fileno()).st_size)
-update_interval = 250 if xmlfilesize > 1e7 else 100
+update_interval = 100 if xmlfilesize < 1e7 else 250 if xmlfilesize < 1e9 else 500
 
 for event, elem in context:
 	if event == 'end' and elem.tag[url_length:] == 'text' and elem.text != None:
@@ -77,9 +102,11 @@ for event, elem in context:
 
 		root.clear()
 
-counter[''] = 0
-counter["''"] = 0
-counter["'"] = 0
+config['excluded'].extend(['', "''", "'"])
+
+for word in config['excluded']:
+	del counter[word]
 
 for keypair in counter.most_common(words_to_output):
 	print(keypair[0])
+
